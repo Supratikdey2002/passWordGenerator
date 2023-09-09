ba759adda3c1f3e8302d40db5d1d6df5f19530eb
Supratik Dey <97463032+Supratikdey2002@users.noreply.github.com>
2023-09-09 21:54:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+const allchar = lowerCase + upperCase + number + symbol;
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * upperCase.length)];
+    password += number[Math.floor(Math.random() * upperCase.length)];
+    password += symbol[Math.floor(Math.random() * upperCase.length)];
+
+    while (length > password.length) {
+        password += allchar[Math.floor(Math.random() * allchar.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand("copy");
+}
